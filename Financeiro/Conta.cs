2a0358d689fbd7d04bb166ecf459fb7cfad1ccc7
vitormfrey <vitormoises.rey@gmail.com>
2021-05-21 08:13:52 -0300

    Primeiro commit C#

---
+++ Financeiro/Conta.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financeiro
+{
+    class Conta
+    {
+        public string titular = "";
+        public int numeroConta = 1;
+        public Double saldo = 0;
+    
+    
+        public Boolean Sacar(Double valor)
+        {
+            if (this.saldo >= valor)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Despositar( Double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public void Transfere(Double valor, Conta c)
+        {
+            if (Sacar(valor))
+                c.Despositar(valor);
+            
+        }
+    }
+}
---
+++ Financeiro/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Financeiro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conta v = new Conta();
+
+            Conta c = new Conta();
+
+            v.Despositar(100);
+            c.Despositar(0);
+
+            v.Transfere(34, c);
+
+            MessageBox.Show(c.saldo.ToString("C"));
+
+        }
+    }
+}
